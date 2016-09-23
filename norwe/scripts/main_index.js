be1c9f17b9b121c7b1544f60c2aa903a9fa4af33
PJCHENder <pjchender@gmail.com>
2016-09-23 16:07:06 +0800

    修改 index 頁面

--- norwe/scripts/main_index.js
+++ norwe/scripts/main_index.js
@@ -1,35 +1,52 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
     var a = 1;
-    var screenWidth = screen.width;
-    $(".logo").on('click', function(){
+    var screenWidth = $(document).width();
+    $(".logo").on('click', function () {
         console.log(this);
-        
         // 點下後收合
-        
-        if(a === 0){
-            $("nav").animate({
-                "height":"160px",
-                "top":"200px"
-            })
-            $(".logo").css("margin","20px auto 0");
-            $("hr").css("display","none");
-            $(".menu").css("display","none");
-            a = 1;
-        
-        // 點下後展開
-        }else if(a === 1){
-            $("nav").animate({
-                "height":"100vh",
-                "top":"0"
-            })
-            if(screenWidth > 1680){
-                $(".logo").css("margin","90px auto");
-            }else{
-                $(".logo").css("margin","30px auto");
+        if (a === 0) {
+            if (screenWidth > 1200) {
+                $(".nav").animate({
+                    "height": "160px"
+                    , "top": "200px"
+                })
+                $(".logo").css("margin", "20px auto 0");
             }
-            $("hr").css("display","block");
-            $(".menu").css("display","block");
+            else if (screenWidth > 480) {
+                $(".nav").animate({
+                    "width": "100px"
+                    , "height": "128px"
+                    , "top": "200px"
+                })
+                $(".logo").css({
+                    "margin": "0 auto"
+                    , "background-size": "50%"
+                });
+            }
+            else {
+                $(".nav").animate({
+                    "width": "100px"
+                    , "height": "128px"
+                    , "top": "0"
+                })
+                $(".logo").css({
+                    "margin": "0 auto"
+                    , "background-size": "50%"
+                });
+            }
+            $(".nav hr").css("display", "none");
+            $(".menu").css("display", "none");
+            a = 1;
+        } // 點下後展開
+        else if (a === 1) {
+            $("nav").animate({
+                "height": "100vh"
+                , "top": "0"
+                , "width": "250px"
+            })
+            $(".logo").css("margin", "30px auto");
+            $(".nav hr").css("display", "block");
+            $(".menu").css("display", "block");
             a = 0;
         }
     })
